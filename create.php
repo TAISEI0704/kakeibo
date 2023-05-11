49596e876e39b5f587531484b26f95f83e046c86
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-05-11 13:58:19 +0800

    cssファイル追加

--- create.php
+++ create.php
@@ -30,7 +30,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登録</title>
     <link rel="stylesheet" href="./assets/css/reset.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style_create.css">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
